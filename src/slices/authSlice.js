6278097cdbe54e293f0289eceaed593ad1309504
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-25 22:42:21 +0530

    slice to set the current user and unset once logged out

---
+++ src/slices/authSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        currentUser: null,
+    },
+    reducers: {
+        setCurrentUser: (state,{payload}) =>{
+            state.currentUser = payload.username;
+        },
+        removeCurrentUser: (state, {payload}) =>{
+            state.currentUser = null;
+        }
+    }
+})
+
+export const { setCurrentUser, removeCurrentUser } = authSlice.actions;
+export default authSlice.reducer;
