c82d6b823fba2fcc2f0380541e3c79fd9c412b7f
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-25 22:33:44 +0530

    created routes for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
+import Homepage from './pages/Homepage'
+import CartPage from './pages/CartPage'
+import Counters from './pages/Counters'
+import MenuPage from './pages/MenuPage'
+import { LoginPage, RegisterPage } from './pages/AuthenticationPage'
 
 function App() {
 
   return (
     <>
-      <p className="font-bold">
-        Click
-      </p>
+      <div className="bg-gray-900 min-h-screen text-white">
+        <BrowserRouter >
+          <Routes>
+            <Route path="/" element={ <Homepage/>} />
+            <Route path="/cart" element={ <CartPage/>} />
+            <Route path="/counter" element={ <Counters/>} />
+            <Route path="/menu" element={ <MenuPage/>} />
+            <Route path="/auth/register" element={ <RegisterPage/>} />
+            <Route path="/auth/login" element={ <LoginPage/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
