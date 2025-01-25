0cf1c6965d05c114420fcc05865afc80603094e8
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-26 01:17:41 +0530

    created a call at the frontend to gets all the information og the user if he logged into the system

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,27 @@ import Counters from './pages/Counters'
 import MenuPage from './pages/MenuPage'
 import { LoginPage, RegisterPage } from './pages/AuthenticationPage'
 import Navbar from './component/Navbar'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setCurrentUser } from './slices/authSlice'
 
 function App() {
+  const dispatch = useDispatch();
+  const MAIN_LINK = import.meta.env.VITE_MAIN_API_URL
+  useEffect(()=>{
+    async function me(token){
+      try {
+        const res = await axios.get(`${MAIN_LINK}/users/me`, {
+          headers: { Authorization : `Bearer ${token}`}
+        })
+        dispatch(setCurrentUser({username: res.data.username}));
+      } catch(err) {
+        console.error("User is not logged in into the website")
+      }
+    }
+    me(localStorage.getItem('accessToken'));
+  })
 
   return (
     <>
