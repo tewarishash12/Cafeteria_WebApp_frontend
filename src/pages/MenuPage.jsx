31023eeba9d86213b2238772c37603c9b697052b
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-28 01:01:53 +0530

    Calling the menu from the redux state

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,40 +1,18 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { addItem } from "../slices/cartSlice";
+import React from "react";
+import { useSelector } from "react-redux";
 import MenuCard from "../component/MenuCard";
 
 function MenuPage() {
-    const MAIN_LINK = import.meta.env.VITE_MAIN_API_URL;
-    const [menuItems, setMenuItems] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        async function fetchMenuItems() {
-            try {
-                const res = await axios.get(`${MAIN_LINK}/dish/alldishes`);
-                setMenuItems(res.data);
-                setLoading(false);
-            } catch (err) {
-                console.error(err.message);
-                setLoading(false);
-            }
-        }
-        fetchMenuItems();
-    }, []);
+    const menuItems = useSelector(state => state.dish.filteredMenu);
 
     return (
         <div className="min-h-screen p-6 bg-gray-900">
             <h1 className="text-3xl font-bold text-center mb-6 text-white">Menu</h1>
-            {loading ? (
-                <div className="text-center text-gray-500">Loading...</div>
-            ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {menuItems.map((item, index) => (
-                        <MenuCard key={item._id} item={item} />
-                    ))}
-                </div>
-            )}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {menuItems.map((item) => (
+                    <MenuCard key={item._id} item={item} />
+                ))}
+            </div>
         </div>
     );
 }
