1c8b54ef5f416d63f39127da19a7420d955dd594
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-27 17:51:32 +0530

    Created the base code to call accessToken with the help of refreshToken

---
+++ src/hooks.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+const AUTH_LINK = import.meta.env.VITE_AUTH_API_URL;
+
+function axiosAuthConfig(method, token, url, body){
+    return {
+        method:method,
+        url:url,
+        data:body,
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+}
+
+export function useRetryCall(method){
+    const [loading,setLoading] = useState(false);
+    async function call(url,body) {
+        setLoading(true);
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            return await axios.request(axiosAuthConfig(method, token, url, body));
+        } catch(err) {
+            const errorMsg = err?.response?.data?.error;
+            if(errorMsg !== 'jwt expired')
+                throw err;
+            const refreshToken = localStorage.getItem('refreshToken');
+            const res = await axios.post(`${AUTH_LINK}/auth/token`, {token: refreshToken})
+        }
+    }
+}
