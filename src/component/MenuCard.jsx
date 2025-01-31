490a3c1c3f5797e8c3e3ca4cf0b6d28d176939dc
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-31 09:46:10 +0530

    updated card ui

--- src/component/MenuCard.jsx
+++ src/component/MenuCard.jsx
@@ -38,22 +38,22 @@ function MenuCard({ item }) {
         setDescription(description);
         setPrice(price);
         setAvailability(availability);
-        setIsModalOpen(true); 
+        setIsModalOpen(true);
     }
 
     async function handleEditSave(id) {
         try {
-            const res = await axios.put(`${MAIN_LINK}/dish/id/${id}`, {dish_name: dish_name, description:description, price:price, availability:availability, image:image })
-            dispatch(setCompleteMenu({menu: res.data.dishes}))
-        } catch(err) {
+            const res = await axios.put(`${MAIN_LINK}/dish/id/${id}`, { dish_name: dish_name, description: description, price: price, availability: availability, image: image })
+            dispatch(setCompleteMenu({ menu: res.data.dishes }))
+        } catch (err) {
             console.error(err.message);
         }
         setIsModalOpen(false);
     }
 
-    async function handleDelete(id){
+    async function handleDelete(id) {
         const res = await axios.delete(`${MAIN_LINK}/dish/id/${id}`)
-        dispatch(setCompleteMenu({menu:res.data.dishes}))
+        dispatch(setCompleteMenu({ menu: res.data.dishes }))
     }
 
     return (
@@ -64,15 +64,10 @@ function MenuCard({ item }) {
                 className="w-full h-48 object-cover border-gray-600"
             />
             <div className="p-4">
-                <h2 className="text-lg mb-2 text-teal-400 font-bold">{item.dish_name}</h2>
+                <h2 className="text-lg mb-2 text-green-400 font-bold">{item.dish_name}</h2>
                 <p className="text-gray-400 text-sm mb-2">{item.description}</p>
-                <p className="text-yellow-400 font-semibold mb-2">Price: ₹{item.price}</p>
-                <p
-                    className={`text-sm ${item.availability
-                            ? "text-purple-300 italic"
-                            : "text-red-600 italic"
-                        }`}
-                >
+                <p className="text-purple-400 font-semibold mb-2">Price: ₹{item.price}</p>
+                <p className={`text-sm ${item.availability ? "text-green-300 italic" : "text-pink-400 italic"}`}>
                     {item.availability ? "Available" : "Out of Stock"}
                 </p>
                 <p className="text-gray-400 text-sm mt-2">
@@ -80,7 +75,7 @@ function MenuCard({ item }) {
                 </p>
                 <div className="flex flex-wrap gap-3 mt-4">
                     <button
-                        className={`${item.availability ? "bg-teal-500 hover:bg-teal-600 text-white cursor-pointer" : "bg-gray-500 hover:bg-gray-600 text-white cursor-not-allowed"}  px-4 py-2 rounded-lg transition`}
+                        className={`${item.availability ? "bg-green-500 hover:bg-green-600 cursor-pointer" : "bg-gray-500 hover:bg-gray-600 cursor-not-allowed"} text-white  px-4 py-2 rounded-lg transition`}
                         onClick={(e) => handleAddToCart(e, item)}
                         disabled={!item.availability}
                     >
@@ -93,7 +88,7 @@ function MenuCard({ item }) {
                         Edit
                     </button>
                     <button
-                        className="bg-red-400 hover:bg-red-500 text-gray-900 px-4 py-2 rounded-lg transition"
+                        className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded-lg transition"
                         onClick={() => handleDelete(item._id)}
                     >
                         Delete
@@ -112,7 +107,7 @@ function MenuCard({ item }) {
                             type="text"
                             name="dish_name"
                             value={dish_name}
-                            onChange={(e)=> setDishName(e.target.value)}
+                            onChange={(e) => setDishName(e.target.value)}
                             className="w-full px-3 py-2 border rounded-lg text-black"
                         />
                     </div>
@@ -122,7 +117,7 @@ function MenuCard({ item }) {
                         <textarea
                             name="description"
                             value={description}
-                            onChange={(e)=> setDescription(e.target.value)}
+                            onChange={(e) => setDescription(e.target.value)}
                             className="w-full px-3 py-2 border rounded-lg text-black"
                         />
                     </div>
@@ -133,7 +128,7 @@ function MenuCard({ item }) {
                             type="number"
                             name="price"
                             value={price}
-                            onChange={(e)=> setPrice(e.target.value)}
+                            onChange={(e) => setPrice(e.target.value)}
                             className="w-full px-3 py-2 border rounded-lg text-black"
                         />
                     </div>
@@ -144,7 +139,7 @@ function MenuCard({ item }) {
                             type="text"
                             name="image_url"
                             value={image}
-                            onChange={(e)=> setImage(e.target.value)}
+                            onChange={(e) => setImage(e.target.value)}
                             className="w-full px-3 py-2 border rounded-lg text-black"
                         />
                     </div>
@@ -168,7 +163,7 @@ function MenuCard({ item }) {
                     </div>
                     <button
                         type="button"
-                        onClick={()=>handleEditSave(item._id)}
+                        onClick={() => handleEditSave(item._id)}
                         className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
                     >
                         Save
