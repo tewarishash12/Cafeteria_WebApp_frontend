c2d6bb8f5d026e086d5b918f3283505058becae7
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-31 12:23:57 +0530

    Created a form to add merchant

---
+++ src/component/AddMerchant.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+function AddMerchant() {
+    const MAIN_LINK = import.meta.env.VITE_MAIN_API_URL;
+
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNo, setPhoneNo] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit() {
+        e.preventDefault();
+        try {
+            await axios.post(`${MAIN_LINK}/users`, { username, email, phoneNo, password }, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` }
+            })
+            console.log("Merchant created successfully");
+
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="space-y-4 text-black">
+            <div>
+                <label className="block text-sm font-medium">Username</label>
+                <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="w-full p-2 border rounded"
+                />
+            </div>
+            <div>
+                <label className="block text-sm font-medium">Email</label>
+                <textarea
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full p-2 border rounded"
+                ></textarea>
+            </div>
+            <div>
+                <label className="block text-sm font-medium">Phone No</label>
+                <textarea
+                    name="phoneNo"
+                    value={phoneNo}
+                    onChange={(e) => setPhoneNo(e.target.value)}
+                    className="w-full p-2 border rounded"
+                ></textarea>
+            </div>
+            <div>
+                <label className="block text-sm font-medium">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full p-2 border rounded"
+                />
+            </div>
+            <button
+                type="submit"
+                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+                Add Merchant
+            </button>
+        </form>
+    )
+}
+
+export default AddMerchant
