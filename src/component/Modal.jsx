1a4633cea2c3da81372faeacc49269e951ec1851
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-02 23:17:40 +0530

    Changed the Modal styling

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 function Modal({ isOpen, onClose, children }) {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-            <div
-                className="bg-gray-200 p-6 rounded-lg shadow-xl w-11/12 max-w-lg"
+        <div
+            className={`fixed inset-0 flex items-center justify-center backdrop-blur-sm z-50`}
+        >
+            <button
+                onClick={onClose}
+                className={`hover:text-gray-700 dark:hover:text-gray-300 absolute top-20 right-10`}
             >
-                <button
-                    onClick={onClose}
-                    className="absolute top-2 right-2 bg-gray-300 text-black rounded-full p-1 hover:bg-gray-400"
-                >
-                    ✖
-                </button>
+                <FaTimes />
+            </button>
+
+            <div
+                className={`bg-white p-6 rounded-lg shadow-lg min-w-[800px] max-h-[80vh] max-w-md m-auto overflow-auto`}>
                 {children}
-            </div>
-        </div>
+            </div >
+        </div >
+
     );
 }
 
