fc523a405f9924d800ba77252c701a7e4d1b398c
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-03 01:32:08 +0530

    updating state in the redux

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -10,9 +10,21 @@ const userSlice = createSlice({
         allUsersList: (state, { payload }) => {
             state.customerList = payload.customers;
             state.merchantList = payload.merchants;
+        },
+        setMerchants: (state, {payload}) =>{
+            const newMerchant = state.customerList.find((data) => data._id === payload.id)
+            newMerchant.role = "merchant";
+            state.customerList = state.customerList.filter((customer) => customer._id !== payload.id);
+            state.merchantList.push(newMerchant);
+        },
+        setCustomers: (state, {payload}) =>{
+            const newCustomer = state.merchantList.find((data)=> data._id === payload.id)
+            newCustomer.role = "customer";
+            state.merchantList = state.merchantList.filter((customer) => customer._id !== payload.id);
+            state.customerList.push(newCustomer);
         }
     }
 })
 
-export const { allUsersList } = userSlice.actions
+export const { allUsersList,setMerchants,setCustomers } = userSlice.actions
 export default userSlice.reducer;
