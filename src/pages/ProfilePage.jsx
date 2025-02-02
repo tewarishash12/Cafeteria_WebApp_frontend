ada0e889d688d9584f79d11697ee6fc484148419
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-02 23:18:20 +0530

    removed unwanted imports and useState

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ProfileInformation from '../component/ProfileInformation';
-import Modal from '../component/Modal';
-import AddMerchant from '../component/AddMerchantButton';
 import Sidebar from '../component/Sidebar';
 
 function ProfilePage() {
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white">
