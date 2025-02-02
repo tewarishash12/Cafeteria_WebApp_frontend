ebfbde27c3bc5e6f9616c3724ee262cf84e9c4cd
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-02 21:21:55 +0530

    initlaised userSlice in redux store

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import authReducer from "../slices/authSlice"
 import cartReducer from "../slices/cartSlice"
 import dishReducer from "../slices/dishSlice"
 import counterReducer from "../slices/counterSlice"
+import userReducer from "../slices/userSlice"
 
 export default configureStore({
     reducer:{
         auth: authReducer,
         cart: cartReducer,
         dish: dishReducer,
-        counter: counterReducer
+        counter: counterReducer,
+        user:userReducer
     }
 })
