cf47939681f61387910a1c6b02c4ef916edfeafc
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-05 12:06:58 +0530

    showing items based on role of logged in user

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -5,7 +5,8 @@ import CounterForm from '../component/Counter/CounterForm';
 import Modal from '../component/Modal/Modal';
 
 function CounterPage() {
-    const counters = useSelector(state => state?.counter?.allCounters)
+    const counters = useSelector(state => state?.counter?.allCounters);
+    const userRole = useSelector(state => state?.auth?.currentUser?.role);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
@@ -14,12 +15,16 @@ function CounterPage() {
 
             {counters?.length > 0 && (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                    <button
-                        onClick={() => setIsModalOpen(true)}
-                        className="border-l-4  border-blue-600 rounded-lg shadow-md bg-slate-200 flex items-center justify-center hover:shadow-xl hover:bg-slate-300"
-                    >
-                        <span className="text-2xl font-bold">Add Counter</span>
-                    </button>
+                    {userRole==="admin" ?
+                        <button
+                            onClick={() => setIsModalOpen(true)}
+                            className="border-l-4  border-blue-600 rounded-lg shadow-md bg-slate-200 flex items-center justify-center hover:shadow-xl hover:bg-slate-300"
+                        >
+                            <span className="text-2xl font-bold">Add Counter</span>
+                        </button>
+                        :
+                        ""
+                    }
                     {counters?.map((counter) => (
                         <CounterCard key={counter?._id} counter={counter} />
                     ))}
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -5,19 +5,22 @@ import FoodForm from "../component/Menu/FoodForm";
 import Modal from "../component/Modal/Modal";
 
 function MenuPage() {
-    const menuItems = useSelector(state => state.dish.fullMenu);
+    const menuItems = useSelector(state => state?.dish?.fullMenu);
+    const userRole = useSelector(state => state?.auth?.currentUser?.role);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
         <div className="min-h-screen p-6">
             <h1 className="text-3xl font-bold text-center mb-4">Menu</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {userRole==="merchant" ? 
                 <button
                     onClick={() => setIsModalOpen(true)}
                     className="border rounded-lg shadow-xl bg-slate-200 flex items-center justify-center hover:shadow-lg hover:bg-blue-200"
                 >
                     <span className="text-2xl font-bold">Add Dish</span>
                 </button>
+                : "" }
                 {menuItems.map((item) => (
                     <MenuCard key={item._id} item={item} />
                 ))}
